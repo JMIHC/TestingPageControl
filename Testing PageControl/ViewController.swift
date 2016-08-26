810fbb1103475af760a1bc9cfaed1ac62588d2fe
jmihc <john@bahai.us>
2016-08-26 11:13:16 -0500

    create and implement PageControl for understanding it

--- Testing PageControl/ViewController.swift
+++ Testing PageControl/ViewController.swift
@@ -10,16 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var colorView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        configurePageControl()
+        pageControlValueDidChange()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    let colors = [
+        UIColor.blackColor(),
+        UIColor.redColor(),
+        UIColor.blueColor(),
+        UIColor.greenColor(),
+        UIColor.cyanColor()
+    ]
+    func configurePageControl() {
+        // The total number of pages that are available is based on how many available colors we have.
+        pageControl.numberOfPages = colors.count
+        pageControl.currentPage = 2
+        
+        pageControl.tintColor = UIColor.orangeColor()
+        pageControl.pageIndicatorTintColor = UIColor.magentaColor()
+        pageControl.currentPageIndicatorTintColor = UIColor.yellowColor()
+        
+        pageControl.addTarget(self, action: #selector(ViewController.pageControlValueDidChange), forControlEvents: .ValueChanged)
+    }
+    func pageControlValueDidChange() {
+        
+        colorView.backgroundColor = colors[pageControl.currentPage]
+    }
+    
 
 }
 
